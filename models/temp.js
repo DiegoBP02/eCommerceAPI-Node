6cba38682842f88ceb313adb8da92b2a2b701f23
Diego Batista Pereira <diegobpcelular@gmail.com>
2022-11-17 22:21:27 -0300

    first commit

---
+++ models/temp.js
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+import { ObjectId } from "mongodb";
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    $match: {
+      product: new ObjectId("6374f70ab418950353259404"),
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      averageRating: {
+        $avg: "$rating",
+      },
+      numOfReviews: {
+        $sum: 1,
+      },
+    },
+  },
+];
+
+const client = await MongoClient.connect("", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const coll = client.db("").collection("");
+const cursor = coll.aggregate(agg);
+const result = await cursor.toArray();
+await client.close();
